c6f583aa0b6bba251539928f07a38c2414da07b3
8connor <connorh16@gmail.com>
2020-10-09 10:44:39 -0400

    Made some comments. Added express-session.

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+// This package will help us keep track of user sessions.
+const session = require("express-session");
+
 //routes being brought in from different file. Recognizes index.js.
-const routes = require("./routes");
+const {News, User, Contact} = require("./routes");
 
 mongoose.connect(
   `mongodb://localhost:27017/gradeBotv2`,
@@ -16,22 +19,27 @@ mongoose.connect(
     console.log("Successfully connected to Database");
   }
 );
-
+// Cross origin requests.
 app.use(cors());
+
 app.use(express.json());
 
-app.use("/api", routes.News);
-app.use("/api", routes.User);
-app.use("/api", routes.Contact);
+//api routes for our api calls.
+app.use("/api", News);
+app.use("/api", User);
+app.use("/api", Contact);
 
+// If none of the routes are hit it sends the static web page.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
+// Creating a server for use with socket io.
 const server = http.createServer(app);
-
+// Defining socket.io
 const io = require("socket.io")(server);
 
+// When you connect it will listen for a chat message. if you message in the chat it will emit that message.
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
